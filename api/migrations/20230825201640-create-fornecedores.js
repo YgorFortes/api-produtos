149dc0a24dd7174a5853bf857d88bec67e5f44cc
Ygor Fortes <ygor1888@hotmail.com>
2023-08-25 17:18:14 -0300

    Adionando modelo de fornecedor

---
+++ api/migrations/20230825201640-create-fornecedores.js
@@ -0,0 +1,41 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Fornecedores', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nome: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      endereco: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      telefone: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      cnpj: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Fornecedores');
+  }
+};
---
+++ api/models/fornecedores.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Fornecedores extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Fornecedores.init({
+    nome: DataTypes.STRING,
+    endereco: DataTypes.STRING,
+    telefone: DataTypes.STRING,
+    cnpj: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Fornecedores',
+  });
+  return Fornecedores;
+};
